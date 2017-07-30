41e2a7ec1417d7ecdf46973334c12ebbee1cc74d
Hideo Hattori <hattori-h@klab.com>
2017-07-30 20:57:45 +0900

    chapter 11

---
+++ src/main11.rs
@@ -0,0 +1,74 @@
+#[macro_use]
+mod utils;
+
+use utils::vec3::{Vec3, unit_vector};
+use utils::ray::Ray;
+use utils::hitable::{Hitable, HitableList, HitRecord};
+use utils::sphere::Sphere;
+use utils::camera::Camera;
+use utils::material::{DummyMat, Lambertian, Metal, Dielectric};
+use utils::random::drand48;
+
+fn color(r: &Ray, world: &Hitable, depth: i32) -> Vec3 {
+    let mut rec = HitRecord::new(Box::new(DummyMat::new()));
+    if world.hit(r, 0.001, std::f32::MAX, &mut rec) {
+        let v1 = Vec3::new(0., 0., 0.);
+        let v2 = Vec3::new(0., 0., 0.);
+        let mut scattered = Ray::new(&v1, &v2);
+        let mut attenuation = Vec3::new(0., 0., 0.);
+        if depth < 50 && rec.mat.as_ref().scatter(r, &rec, &mut attenuation, &mut scattered) {
+            return attenuation * color(&scattered, world, depth + 1);
+        }
+        return Vec3::new(0., 0., 0.);
+    }
+    let unit_direction = unit_vector(r.direction().clone());
+    let t: f32 = 0.5 * (unit_direction.y() + 1.);
+    Vec3::new(1., 1., 1.) * (1. - t) + Vec3::new(0.5, 0.7, 1.) * t
+}
+
+fn main() {
+    let nx = 200;
+    let ny = 100;
+    let ns = 100;
+    println!("P3\n{} {}\n255", nx, ny);
+
+    let s1 = get_sphere!(Lambertian, Vec3::new(0.1, 0.2, 0.5), Vec3::new(0., 0., -1.), 0.5);
+    let s2 = get_sphere!(Lambertian, Vec3::new(0.8, 0.8, 0.), Vec3::new(0., -100.5, -1.), 100.);
+    let s3 = get_sphere!(Metal, Vec3::new(0.8, 0.6, 0.2), 0.1, Vec3::new(1., 0., -1.), 0.5);
+    let s4 = get_sphere!(Dielectric, 1.5, Vec3::new(-1., 0., -1.), 0.5);
+    let s5 = get_sphere!(Dielectric, 1.5, Vec3::new(-1., 0., -1.), -0.45);
+    let world: HitableList = HitableList { list: vec![s1, s2, s3, s4, s5] };
+
+    let lookfrom = Vec3::new(3., 3., 2.);
+    let lookat = Vec3::new(0., 0., -1.);
+    let dist_to_focus = (lookfrom.clone() - lookat.clone()).len();
+    let aperture: f32 = 2.;
+    let mut cam = Camera::with_focus(&lookfrom,
+                                     &lookat,
+                                     &Vec3::new(0., 1., 0.),
+                                     20.,
+                                     nx as f32 / ny as f32,
+                                     aperture,
+                                     dist_to_focus);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let mut col = Vec3::new(0., 0., 0.);
+            for _ in 0..ns {
+                let u: f32 = (i as f32 + drand48()) / nx as f32;
+                let v: f32 = (j as f32 + drand48()) / ny as f32;
+                let r = cam.get_ray(u, v);
+
+                // let p = r.point_at_parameter(2.); // NOTE: not use?
+
+                col = col + color(&r, &world, 0);
+            }
+            col = col / ns as f32;
+            col = Vec3::new(col.e.0.sqrt(), col.e.1.sqrt(), col.e.2.sqrt());
+            let ir = (255.99 * col.e.0) as i32;
+            let ig = (255.99 * col.e.1) as i32;
+            let ib = (255.99 * col.e.2) as i32;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
--- src/utils/camera.rs
+++ src/utils/camera.rs
@@ -1,6 +1,7 @@
 use std::f32::consts::PI;
-use utils::vec3::{Vec3, unit_vector, cross};
+use utils::vec3::{Vec3, unit_vector, cross, dot};
 use utils::ray::Ray;
+use utils::random::drand48;
 
 #[derive(Default)]
 pub struct Camera {
@@ -8,6 +9,10 @@ pub struct Camera {
     pub lower_left_corner: Vec3,
     pub horizontal: Vec3,
     pub vertical: Vec3,
+    pub u: Vec3,
+    pub v: Vec3,
+    pub w: Vec3,
+    pub lens_radius: f32,
 
     #[allow(dead_code)]
     ray_vec: Vec3,
@@ -20,6 +25,7 @@ impl Camera {
             lower_left_corner: Vec3::new(-2., -1., -1.),
             horizontal: Vec3::new(4., 0., 0.),
             vertical: Vec3::new(0., 2., 0.),
+            lens_radius: 1.,
             ..Self::default()
         }
     }
@@ -32,6 +38,7 @@ impl Camera {
             lower_left_corner: Vec3::new(-half_width, -half_height, -1.),
             horizontal: Vec3::new(2. * half_width, 0., 0.),
             vertical: Vec3::new(0., 2. * half_height, 0.),
+            lens_radius: 1.,
             ..Self::default()
         }
     }
@@ -53,9 +60,58 @@ impl Camera {
         }
     }
 
-    pub fn get_ray(&mut self, u: f32, v: f32) -> Ray {
-        self.ray_vec = self.lower_left_corner.clone() + self.horizontal.clone() * u + self.vertical.clone() * v -
-                       self.origin.clone();
-        Ray::new(&self.origin, &self.ray_vec)
+    pub fn with_focus(lookfrom: &Vec3,
+                      lookat: &Vec3,
+                      vup: &Vec3,
+                      vfov: f32,
+                      aspect: f32,
+                      aperture: f32,
+                      focus_dist: f32)
+                      -> Self {
+        let lens_radius = aperture / 2.;
+        let w: Vec3 = unit_vector(lookfrom.clone() - lookat.clone());
+        let u: Vec3 = unit_vector(cross(vup, &w));
+        let v: Vec3 = cross(&w, &u);
+        let theta = vfov * PI / 180.;
+        let half_height = (theta / 2.).tan();
+        let half_width = aspect * half_height;
+        let origin = lookfrom.clone();
+        Self {
+            origin: origin.clone(),
+            lower_left_corner: origin - u.clone() * focus_dist * half_width - v.clone() * focus_dist * half_height -
+                               w.clone() * focus_dist,
+            horizontal: u.clone() * 2. * focus_dist * half_width,
+            vertical: v.clone() * 2. * focus_dist * half_height,
+            w: w,
+            u: u,
+            v: v,
+            lens_radius: lens_radius,
+            ..Self::default()
+        }
     }
+
+    pub fn get_ray(&mut self, u: f32, v: f32) -> Ray {
+        // self.ray_vec = self.lower_left_corner.clone() + self.horizontal.clone() * u + self.vertical.clone() * v -
+        //                self.origin.clone();
+        // Ray::new(&self.origin, &self.ray_vec)
+
+        let rd = random_in_unit_disk() * self.lens_radius;
+        let offset: Vec3 = self.u.clone() * rd.x() + self.v.clone() * rd.y();
+        self.ray_vec = self.lower_left_corner.clone() + self.horizontal.clone() * u + self.vertical.clone() * v -
+                       self.origin.clone() - offset.clone();
+        let origin = self.origin.clone() + offset.clone();
+        Ray::new(&origin, &self.ray_vec)
+    }
+}
+
+#[allow(dead_code)]
+pub fn random_in_unit_disk() -> Vec3 {
+    let mut p: Vec3;
+    loop {
+        p = Vec3::new(drand48(), drand48(), 0.) * 2.0 - Vec3::new(1., 1., 0.);
+        if !(dot(&p, &p) >= 1.) {
+            break;
+        }
+    }
+    p
 }
